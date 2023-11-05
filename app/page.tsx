f2f24a7a2f8970f62aee0dac123669aa8dbc5dbf
Iwona Behnke <behnke.iwona@gmail.com>
2023-11-05 22:07:07 +0100

    Homepage - top section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className='px-6 border-2 md:px-20 py-24'>
+      <div className='flex max-xl:flex-col gap-16'>
+        <div className='flex flex-col justify-center '>
+          <p className='small-text'>
+            Discover More, Spend Less
+            <Image
+              src="assets/icons/arrow-right.svg"
+              alt='arrow-right'
+              width={16}
+              height={16}
+            />
+          </p>
+          <h1 className='head-text'>
+            Your Savings Journey Starts with <span className='text-primary'>Data</span>
+          </h1>
+          <p className='mt-6'>Our app harnesses the power of web scraping to provide you with real-time data on your favorite products. Discover price trends, track changes over time, and receive alerts when prices hit their lowest point – all at your fingertips.</p>
+          Searchbar
+        </div>
+        HeroCarousel
+      </div>
+    </section>
+
+
+    </>
   )
 }
 
