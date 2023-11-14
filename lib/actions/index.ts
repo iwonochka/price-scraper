22836dba5c67764568a2702cc243c2b5f8de583e
Iwona Behnke <behnke.iwona@gmail.com>
2023-11-14 15:43:52 +0100

    MongoDB connection

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl:string) {
@@ -7,11 +8,12 @@ export async function scrapeAndStoreProduct(productUrl:string) {
     console.log("No productUrl in scrapeAndStoreProduct");
     return;
   }
-  console.log("scrapeAndStoreProduct before ty block");
+  console.log("scrapeAndStoreProduct before try block");
   try {
+    connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
     if (!scrapedProduct) return;
-    
+
     console.log("scrapedProduct from try block:", scrapedProduct);
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
